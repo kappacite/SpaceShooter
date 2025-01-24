27bbb6376a33fccd06b59d706778da4e3c7e658e
kappacite <robyn.denoeud@lacatholille.fr>
2025-01-24 11:11:40 +0100

    Added ModuleData
    Added ModuleData

---
+++ Assets/Scripts/ModuleData.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ModuleData", menuName = "ScriptableObject/Player/ModuleData")]
+public class ModuleData : ScriptableObject
+{
+    public int price;
+    public string name;
+    public Sprite sprite;
+    public RuntimeAnimatorController animatorController;
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,10 +10,26 @@ public class PlayerController : MonoBehaviour
     private float _shield;
     private float _maxShield;
     private float _gold;
+
+    public GameObject engine;
+    public GameObject weapon;
+
+    public ModuleData engineData;
+    public ModuleData weaponData;
     // Start is called before the first frame update
     void Start()
     {
-        
+
+        engine = GameObject.FindGameObjectWithTag("Engine");
+        weapon = GameObject.FindGameObjectWithTag("Weapon");
+
+        engine.GetComponentInChildren<Animator>().runtimeAnimatorController = engineData.animatorController;
+        engine.GetComponent<SpriteRenderer>().sprite = engineData.sprite;
+
+        weapon.GetComponent<Animator>().runtimeAnimatorController = weaponData.animatorController;
+        weapon.GetComponent<SpriteRenderer>().sprite = weaponData.sprite;
+
+
     }
 
     // Update is called once per frame
