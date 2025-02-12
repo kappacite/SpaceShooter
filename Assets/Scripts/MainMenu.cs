99ab394bdcdd99779131392e85c7b8344007885c
Stitchmod <tom2004.eisenschmidt@gmail.com>
2025-02-12 11:50:00 +0100

    MainMenu & sound

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+    void Start(){
+
+    }
+
+    void Update(){
+        
+    }
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("MainScene"); 
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit(); 
+    }
+}
