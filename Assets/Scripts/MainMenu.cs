0e1641a24c364727d6883f0b1e0e4c15ed07870b
Stitchmod <tom2004.eisenschmidt@gmail.com>
2025-02-26 11:51:02 +0100

    menu terminer

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -20,6 +20,10 @@ public class MainMenu : MonoBehaviour
 
     public void QuitGame()
     {
-        Application.Quit(); 
+    #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false; 
+    #else
+        Application.Quit();
+    #endif 
     }
 }
